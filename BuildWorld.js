d3762d9672f1fadab6c3c5d156272c61d18e9267
Jack Twilley <mathuin@gmail.com>
2011-01-24 23:20:14 -0800

    First real working version.

---
+++ BuildWorld.js
@@ -0,0 +1,170 @@
+﻿print('Begin RealMC demo');
+
+// Load an image object
+var lcimg = JCraft.Components.Images.Load("lcimage.gif");
+var elevimg = JCraft.Components.Images.Load("elevimage.gif");
+
+var latrange = lcimg.height;
+var longrange = lcimg.width;
+
+var sealevel = 64;
+var baseline = 50; // below here is just stone
+var filler = sealevel - baseline;
+
+// set maxMapHeight to a conservative value
+var maxMapHeight = 125 - sealevel;
+
+map.fillBlocks(0, latrange, 0, baseline, 0, longrange, blockTypes.Stone);
+
+//fill bottom and sides with bedrock
+map.fillBlocks(0, latrange, 0, 1, 0, longrange, blockTypes.Bedrock);
+
+map.fillBlocks(0, latrange, 0, sealevel+1, 0, 1, blockTypes.Bedrock);
+map.fillBlocks(0, latrange, 0, sealevel+1, longrange, 1, blockTypes.Bedrock);
+map.fillBlocks(0, 1, 0, sealevel+1, 0, longrange, blockTypes.Bedrock);
+map.fillBlocks(latrange, 1, 0, sealevel+1, 0, longrange, blockTypes.Bedrock);
+
+var lcval = 0;
+var elevval = 0;
+var maxelev = 0;
+var spawnx = 0;
+var spawny = 0;
+var spawnz = 0;
+
+// fills a column with a base block type then covers it with a block type
+function two_layer(x, z, elevval, depth, baseblockType, topblockType) {
+    underlayer = elevval-depth;
+    map.fillBlocks(x, 1, baseline, filler+underlayer, z, 1, baseblockType);
+    map.fillBlocks(x, 1, sealevel+underlayer, depth, z, 1, topblockType);
+}
+
+// fills a column with a single block type 
+function one_layer(x, z, elevval, blockType) {
+    map.fillBlocks(x, 1, baseline, filler+elevval, z, 1, blockType);
+}
+
+for (x = 0; x < latrange; x++) {
+    for (z = 0; z < longrange; z++) {
+	// flipping it backwards because Z increases west not east
+	flatindex = (x * longrange) + (longrange - z - 1);
+	lcval = lcimg[flatindex];
+	elevval = elevimg[flatindex];
+	if (elevval > maxMapHeight) {
+	    print('oh no elevation ' + elevval + ' is too high');
+	    elevval = maxMapHeight;
+	}
+	if (elevval > maxelev) {
+	    maxelev = elevval;
+	    spawnx = x;
+	    spawnz = z;
+	    spawny = sealevel+elevval;
+	}
+	switch(lcval) {
+	case 11:
+	    // water 3m over sand
+	    two_layer(x, z, elevval, 3, blockTypes.Sand, blockTypes.Water);
+	    break;
+	case 12:
+	    // ice
+	    // FIXME: how to put ice on?
+	    two_layer(x, z, elevval, 3, blockTypes.Sand, blockTypes.Water);
+	    break;
+	case 21:
+	    // developed/open-space (20% stone 80% grass rand tree)
+	case 22:
+	    // developed/open-space (35% stone 65% grass rand tree)
+	case 23:
+	    // developed/open-space (65% stone 35% grass rand tree)
+	case 24:
+	    // developed/open-space (90% stone 10% grass rand tree)
+	    one_layer(x, z, elevval, blockTypes.Stone);
+	    break;
+	case 31:
+	    // barren land (baseline% sand baseline% stone)
+	case 32:
+	    // unconsolidated shore (sand)
+	    one_layer(x, z, elevval, blockTypes.Sand);
+	    break;
+	case 41:
+	    // deciduous forest (grass with tree #1)
+	case 42:
+	    // evergreen forest (grass with tree #2)
+	case 43:
+	    // evergreen forest (grass with either tree)
+	    // FIXME: how to put grass on dirt?
+	    one_layer(x, z, elevval, blockTypes.Dirt);
+	    break;
+	case 51:
+	    // dwarf scrub (grass with 25% stone)
+	case 52:
+	    // shrub/scrub (grass with 25% stone)
+	    // FIXME: how to put grass on dirt?
+	    one_layer(x, z, elevval, blockTypes.Dirt);
+	    break;
+	case 71:
+	    // grasslands/herbaceous
+	case 72:
+	    // sedge/herbaceous
+	    // FIXME: how to put grass on dirt?
+	    one_layer(x, z, elevval, blockTypes.Dirt);
+	    break;
+	case 73:
+	    // lichens (90% stone 10% grass)
+	case 74:
+	    // moss (90% stone 10% grass)
+	    one_layer(x, z, elevval, blockTypes.Stone);
+	    break;
+	case 81:
+	    // pasture/hay
+	case 82:
+	    // cultivated crops
+	    // FIXME: how to put grass on dirt?
+	    one_layer(x, z, elevval, blockTypes.Dirt);
+	    break;
+	case 90:
+	    // woody wetlands (grass with rand trees and -2m water)
+	case 91:
+	    // palustrine forested wetlands (grass with rand trees and -2m water)
+	case 92:
+	    // palustrine scrub/shrub wetlands (grass with baseline% -2m water)
+	case 93:
+	    // estuarine forested wetlands (grass with rand trees and water)
+	case 94:
+	    // estuarine scrub/shrub wetlands (grass with baseline% -2m water)
+	case 95:
+	    // emergent herbaceous wetlands (grass with baseline% -2m water)
+	    one_layer(x, z, elevval, blockTypes.Dirt);
+	    break;
+	case 96:
+	    // palustrine emergent wetlands-persistent (-2m water?)
+	case 97:
+	    // estuarine emergent wetlands (-2m water)
+	case 98:
+	    // palustrine aquatic bed (-2m water)
+	case 99:
+	    // estuarine aquatic bed (-2m water)
+	    two_layer(x, z, elevval, 2, blockTypes.Dirt, blockTypes.Water)
+	    break;
+	default:
+	    print('unexpected value for land cover: ' + lcval);
+	    one_layer(x, z, elevval, blockTypes.Dirt);
+	    break;
+	}
+    }
+}
+
+// set player position and spawn point (in this case, equal)
+print('Setting spawn values: ' + spawnx + ', ' + spawny + ', ' + spawnz);
+map.spawn = { x: spawnx, y: spawny, z: spawnz };
+map.playerLocation = map.spawn;
+
+map.playerInventory.Add(new Item(itemTypes.IronSword));
+map.playerInventory.Add(new Item(itemTypes.IronPickaxe));
+map.playerInventory.Add(new Item(itemTypes.IronShovel));
+map.playerInventory.Add(new Item(itemTypes.IronAxe));
+map.playerInventory.AddAt(new Item(blockTypes.Torch), 8);
+var sandstack = new Item(blockTypes.Sand);
+sandstack.Count = 64;
+map.playerInventory.AddAt(sandstack, 7);
+
+print('End RealMC Demo');
---
+++ mklcelevdata.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# mklcelevdata.py - 2010Jan21 - mathuin@gmail.com
+
+# this script builds arrays for land cover and elevation
+
+from __future__ import division
+import os
+import fnmatch
+import sys
+import struct
+import numpy
+import Image
+from osgeo import gdal
+from osgeo import osr
+from osgeo.gdalconst import *
+from invdisttree import *
+gdal.UseExceptions()
+
+# constants
+#FIXME: (consider retrieving this from transform?)
+lcperpixel = 30 # meters per pixel in landcover file 
+scale = 6 # with 30m data, use 6 or 10 or maybe 30
+
+# functions
+def locateDataset(region, prefix=""):
+    "Given a region name and an optional prefix, returns the dataset for that region."
+    # NB: assumed that exactly one dataset exists for each region/prefix
+    for path, dirs, files in os.walk(os.path.abspath(region)):
+        for filename in fnmatch.filter(files, prefix+"[0-9]*.tif"):
+            dsfilename = os.path.join(path, filename)
+    return gdal.Open(dsfilename, GA_ReadOnly)
+
+def getArrays(lcds, elevds, hScale, vScale=0):
+    "Given roughly coincident datasets for land cover and elevation, this function returns two arrays (land cover and elevation respectively) registered to the land cover locations and scaled if desired."
+    # sigh!
+    if (vScale == 0):
+        vScale = hScale
+    # calculate hMult and check it
+    # FIXME: do after reading in land cover?
+    hMult = lcperpixel//hScale
+    if (hMult % 2 != 1):
+        print "Error: scaling factor must be odd!"
+        sys.exit()
+    # calculate hStep
+    hStep = hMult//2
+
+    # get elevation data
+    (elevT, elevGeoT, elevData) = getTransformsAndData(elevds)
+    elevLatLongList = []
+    elevValue = []
+
+    for rowIndex in range(elevData.shape[0]):
+        for colIndex in range(elevData.shape[1]):
+            (newLat, newLong) = getLatLong(elevT, elevGeoT, rowIndex, colIndex)
+            elevLatLongList.append([newLat, newLong])
+            elevValue.append(elevData[rowIndex][colIndex])
+
+    # check for altitude problems
+    if ((elevData.max()/vScale) > 64):
+        newvScale = elevData.max()//64
+        print "Warning: vertical scale of %d insufficient, using %d instead" % (vScale, newvScale)
+        vScale = newvScale
+
+    # build elevation arrays
+    elevLatLong = numpy.array(elevLatLongList)
+    elevValue = numpy.array(elevValue) / vScale
+
+    # build elevation IDT
+    elevIDT = Invdisttree(elevLatLong, elevValue)
+    
+    # get land cover data
+    (lcT, lcGeoT, lcData) = getTransformsAndData(lcds)
+    # FIXME: newshape will depend on new rectangle
+    lcLatLongList = []
+    lcValueList = []
+    lcSortMeList = []
+
+    # build land cover array
+    for rowIndex in range(lcData.shape[0]):
+        for colIndex in range(lcData.shape[1]):
+            for multRow in range(-1*hStep,hStep+1):
+                for multCol in range(-1*hStep,hStep+1):
+                    newRow = rowIndex + (multRow/hMult)
+                    newCol = colIndex + (multCol/hMult)
+                    (newLat, newLong) = getLatLong(lcT, lcGeoT, newRow, newCol)
+                    lcLatLongList.append([newLat, newLong])
+                    lcValueList.append(lcData[rowIndex][colIndex])
+                    lcSortMeList.append([newRow, newCol])
+
+    # build land cover array
+    lcSortMe = numpy.array(lcSortMeList)
+    lcLatLongInds = numpy.lexsort((lcSortMe[:,1], lcSortMe[:,0]))
+    lcLatLong = numpy.array(lcLatLongList)[lcLatLongInds]
+    lcArr = numpy.array(lcValueList)[lcLatLongInds].reshape(hMult*lcData.shape[0], hMult*lcData.shape[1])
+
+    # compute elevation array from inverse distance tree
+    elevArr = elevIDT(lcLatLong, nnear=8, eps=.1).reshape(hMult*lcData.shape[0], hMult*lcData.shape[1])
+
+    # return the two arrays
+    return lcArr, elevArr
+
+def getTransformsAndData(ds):
+    "Given a dataset, return the transform and geotransform."
+    Projection = ds.GetProjectionRef()
+    Proj = osr.SpatialReference(Projection)
+    LatLong = Proj.CloneGeogCS()
+    Transform = osr.CoordinateTransformation(Proj, LatLong)
+    GeoTransform = ds.GetGeoTransform()
+    Band = ds.GetRasterBand(1)
+    Data = Band.ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize)
+    Band = None
+
+    return Transform, GeoTransform, Data
+
+def getLatLong(transform, geotransform, x, y):
+    "Given transform, geotransform, and coordinates, return latitude and longitude.  Based on GDALInfoReportCorner() from gdalinfo.py"
+    dfGeoX = geotransform[0] + geotransform[1] * x + geotransform[2] * y
+    dfGeoY = geotransform[3] + geotransform[4] * x + geotransform[5] * y
+    pnt = transform.TransformPoint(dfGeoX, dfGeoY, 0)
+    return pnt[1], pnt[0]
+
+# here we go!
+if (len(sys.argv) != 2):
+    region = 'BlockIsland'
+else:
+    region = sys.argv[1]
+lcds = locateDataset(region)
+elevds = locateDataset(region, 'NED_')
+# getArrays with one argument uses the same scale for horiz and vert
+#lcdata, elevdata = getArrays(lcds, elevds, scale)
+# use 30, 6 for testing
+lcdata, elevdata = getArrays(lcds, elevds, 30, 6)
+
+# now that we have the data, let's turn it into happy image stuff
+lcimage = Image.fromarray(lcdata)
+lcimage.save(region+'-test-lcimage.gif')
+elevimage = Image.fromarray(elevdata)
+elevimage.save(region+'-test-elevimage.gif')
