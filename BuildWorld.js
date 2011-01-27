47e0184a40980d81bbfacb9e8266d6e1535a0f76
Jack Twilley <mathuin@gmail.com>
2011-01-27 13:27:06 -0800

    added statistics, added the ability to build a subset of the available tiles

--- BuildWorld.js
+++ BuildWorld.js
@@ -20,22 +20,30 @@ var tilecols = 256;
 
 // what region are we doing?
 var region = 'BlockIsland';
-var maxrows = 304*5;
-var maxcols = 398*5;
+var minrows = 0;
+var mincols = 0;
+var maxrows = 1281;
+var maxcols = 1793;
+
+// land cover associative array for printing
+var lcType = {};
+var lcCount = {};
+var lcTotal = 0;
 
 // inside the loop
-function process_image(offset_x, offset_z) {
-    var lcimg = JCraft.Components.Images.Load("Images/"+region+"-lc-"+offset_x+"-"+offset_z+".gif");
-    var elevimg = JCraft.Components.Images.Load("Images/"+region+"-elev-"+offset_x+"-"+offset_z+".gif");
+function processImage(offset_x, offset_z) {
+    var imagedir = "Images/"+region;
+    var lcimg = JCraft.Components.Images.Load(imagedir+"/lc-"+offset_x+"-"+offset_z+".gif");
+    var elevimg = JCraft.Components.Images.Load(imagedir+"/elev-"+offset_x+"-"+offset_z+".gif");
     var size_x = lcimg.width;
     var size_z = lcimg.height;
     var stop_x = offset_x+size_x;
     var stop_z = offset_z+size_z;
+    var imageDate = new Date();
+    var startTime = imageDate.getTime();
 
-    // debug output
-    print('Image: (' + size_x + ', ' + size_z + ')');
-    print('Offset: (' + offset_x + ', ' + offset_z + ')');
-    print('Stop: (' + stop_x + ', ' + stop_z + ')');
+    // inform the user
+    print('Processing tile at position ('+offset_x+','+offset_z+')...');
 
     // fill bottom of this particular range with bedrock
     map.fillBlocks(offset_x, stop_x, 0, 1, offset_z, stop_z, blockTypes.Bedrock);
@@ -44,13 +52,13 @@ function process_image(offset_x, offset_z) {
     map.fillBlocks(offset_x, stop_x, 1, baseline, offset_z, stop_z, blockTypes.Stone);
 
     // iterate over the image
-    for (x = 0; x < size_x; x++) {
-	for (z = 0; z < size_z; z++) {
-	    flatindex = (z * size_x) + x;
-	    lcval = lcimg[flatindex];
-	    elevval = elevimg[flatindex];
-	    real_x = offset_x + x;
-	    real_z = offset_z + z;
+    for (var x = 0; x < size_x; x++) {
+	for (var z = 0; z < size_z; z++) {
+	    var flatindex = (z * size_x) + x;
+	    var lcval = lcimg[flatindex];
+	    var elevval = elevimg[flatindex];
+	    var real_x = offset_x + x;
+	    var real_z = offset_z + z;
 	    if (elevval > maxMapHeight) {
 		print('oh no elevation ' + elevval + ' is too high');
 		elevval = maxMapHeight;
@@ -61,109 +69,163 @@ function process_image(offset_x, offset_z) {
 		spawnz = real_z;
 		spawny = sealevel+elevval;
 	    }
-	    process_lcval(lcval, real_x, real_z, elevval);
+	    processLcval(lcval, real_x, real_z, elevval)
 	}
+    }
+
+    // print out status
+    var imageDate = new Date();
+    var endTime = imageDate.getTime();
+    var imageDelta = Math.round(endTime - startTime)/1000;
+    print('... finished in ' + imageDelta + ' seconds.')
+}
+
+function populateLandCoverVariables(lcType, lcCount) {
+    // first add all the text values for land covers
+    lcMetaType = {
+	 0 : "Unknown",
+	11 : "Water",
+	12 : "Ice",
+	21 : "Developed/Open-Space <20%",
+	22 : "Developed/Open-Space 20-35%",
+	23 : "Developed/Open-Space 35-60%",
+	24 : "Developed/Open-Space >60%",
+	31 : "Barren Land",
+	32 : "Unconsolidated Shore",
+	41 : "Deciduous Forest",
+	42 : "Evergreen Forest",
+	43 : "Mixed Forest",
+	51 : "Dwarf Scrub",
+	52 : "Shrub/Scrub",
+	71 : "Grasslands/Herbaceous",
+	72 : "Sedge/Herbaceous",
+	73 : "Lichens",
+	74 : "Moss",
+	81 : "Pasture/Hay",
+	82 : "Cultivated Crops",
+	90 : "Woody Wetlands",
+	91 : "Palustrine Forested Wetlands",
+	92 : "Palustrine Scrub/Shrub Wetlands",
+	93 : "Estuarine Forested Wetlands",
+	94 : "Estuarine Scrub/Shrub Wetlands",
+	95 : "Emergent Herbaceous Wetlands",
+	96 : "Palustrine Emergent Wetlands",
+	97 : "Estuarine Emergent Wetlands",
+	98 : "Palustrine Aquatic Bed",
+	99 : "Estuarine Aquatic Bed",
+    };
+    for (var i in lcMetaType) {
+	lcType[i] = lcMetaType[i];
+	lcCount[i] = 0;
     }
 }
 
 // process a given land cover value
-function process_lcval(lcval, x, z,elevval) {
-    switch(lcval) {
-    case 11:
-	// water 3m over sand
-	two_layer(x, z, elevval, 3, blockTypes.Sand, blockTypes.Water);
-	break;
-    case 12:
-	// ice
-	// FIXME: how to put ice on?
-	two_layer(x, z, elevval, 3, blockTypes.Sand, blockTypes.Water);
-	break;
-    case 21:
-	// developed/open-space (20% stone 80% grass rand tree)
-    case 22:
-	// developed/open-space (35% stone 65% grass rand tree)
-    case 23:
-	// developed/open-space (65% stone 35% grass rand tree)
-    case 24:
-	// developed/open-space (90% stone 10% grass rand tree)
-	one_layer(x, z, elevval, blockTypes.Stone);
-	break;
-    case 31:
-	// barren land (baseline% sand baseline% stone)
-    case 32:
-	// unconsolidated shore (sand)
-	one_layer(x, z, elevval, blockTypes.Sand);
-	break;
-    case 41:
-	// deciduous forest (grass with tree #1)
-    case 42:
-	// evergreen forest (grass with tree #2)
-    case 43:
-	// evergreen forest (grass with either tree)
-	// FIXME: how to put grass on dirt?
-	one_layer(x, z, elevval, blockTypes.Dirt);
-	break;
-    case 51:
-	// dwarf scrub (grass with 25% stone)
-    case 52:
-	// shrub/scrub (grass with 25% stone)
-	// FIXME: how to put grass on dirt?
-	one_layer(x, z, elevval, blockTypes.Dirt);
-	break;
-    case 71:
-	// grasslands/herbaceous
-    case 72:
-	// sedge/herbaceous
-	// FIXME: how to put grass on dirt?
-	one_layer(x, z, elevval, blockTypes.Dirt);
-	break;
-    case 73:
-	// lichens (90% stone 10% grass)
-    case 74:
-	// moss (90% stone 10% grass)
-	one_layer(x, z, elevval, blockTypes.Stone);
-	break;
-    case 81:
-	// pasture/hay
-    case 82:
-	// cultivated crops
-	// FIXME: how to put grass on dirt?
-	one_layer(x, z, elevval, blockTypes.Dirt);
-	break;
-    case 90:
-	// woody wetlands (grass with rand trees and -2m water)
-    case 91:
-	// palustrine forested wetlands (grass with rand trees and -2m water)
-    case 92:
-	// palustrine scrub/shrub wetlands (grass with baseline% -2m water)
-    case 93:
-	// estuarine forested wetlands (grass with rand trees and water)
-    case 94:
-	// estuarine scrub/shrub wetlands (grass with baseline% -2m water)
-    case 95:
-	// emergent herbaceous wetlands (grass with baseline% -2m water)
-	one_layer(x, z, elevval, blockTypes.Dirt);
-	break;
-    case 96:
-	// palustrine emergent wetlands-persistent (-2m water?)
-    case 97:
-	// estuarine emergent wetlands (-2m water)
-    case 98:
-	// palustrine aquatic bed (-2m water)
-    case 99:
-	// estuarine aquatic bed (-2m water)
-	two_layer(x, z, elevval, 2, blockTypes.Dirt, blockTypes.Water)
-	    break;
-    default:
+function processLcval(lcval, x, z, elevval) {
+    lcTotal++;
+    if (!(lcval in lcType)) {
 	print('unexpected value for land cover: ' + lcval);
+	lcCount[0]++;
 	one_layer(x, z, elevval, blockTypes.Dirt);
-	break;
+    } else {
+	lcCount[lcval]++;
+	switch(lcval) {
+	case 11:
+	    // water 3m over sand
+	    two_layer(x, z, elevval, 3, blockTypes.Sand, blockTypes.Water);
+	    break;
+	case 12:
+	    // ice
+	    // FIXME: how to put ice on?
+	    two_layer(x, z, elevval, 3, blockTypes.Sand, blockTypes.Water);
+	    break;
+	case 21:
+	    // developed/open-space (20% stone 80% grass rand tree)
+	case 22:
+	    // developed/open-space (35% stone 65% grass rand tree)
+	case 23:
+	    // developed/open-space (65% stone 35% grass rand tree)
+	case 24:
+	    // developed/open-space (90% stone 10% grass rand tree)
+	    one_layer(x, z, elevval, blockTypes.Stone);
+	    break;
+	case 31:
+	    // barren land (baseline% sand baseline% stone)
+	case 32:
+	    // unconsolidated shore (sand)
+	    one_layer(x, z, elevval, blockTypes.Sand);
+	    break;
+	case 41:
+	    // deciduous forest (grass with tree #1)
+	case 42:
+	    // evergreen forest (grass with tree #2)
+	case 43:
+	    // mixed forest (grass with either tree)
+	    // FIXME: how to put grass on dirt?
+	    one_layer(x, z, elevval, blockTypes.Dirt);
+	    break;
+	case 51:
+	    // dwarf scrub (grass with 25% stone)
+	case 52:
+	    // shrub/scrub (grass with 25% stone)
+	    // FIXME: how to put grass on dirt?
+	    one_layer(x, z, elevval, blockTypes.Dirt);
+	    break;
+	case 71:
+	    // grasslands/herbaceous
+	case 72:
+	    // sedge/herbaceous
+	    // FIXME: how to put grass on dirt?
+	    one_layer(x, z, elevval, blockTypes.Dirt);
+	    break;
+	case 73:
+	    // lichens (90% stone 10% grass)
+	case 74:
+	    // moss (90% stone 10% grass)
+	    one_layer(x, z, elevval, blockTypes.Stone);
+	    break;
+	case 81:
+	    // pasture/hay
+	case 82:
+	    // cultivated crops
+	    // FIXME: how to put grass on dirt?
+	    one_layer(x, z, elevval, blockTypes.Dirt);
+	    break;
+	case 90:
+	    // woody wetlands (grass with rand trees and -2m water)
+	case 91:
+	    // palustrine forested wetlands (grass with rand trees and -2m water)
+	case 92:
+	    // palustrine scrub/shrub wetlands (grass with baseline% -2m water)
+	case 93:
+	    // estuarine forested wetlands (grass with rand trees and water)
+	case 94:
+	    // estuarine scrub/shrub wetlands (grass with baseline% -2m water)
+	case 95:
+	    // emergent herbaceous wetlands (grass with baseline% -2m water)
+	    one_layer(x, z, elevval, blockTypes.Dirt);
+	    break;
+	case 96:
+	    // palustrine emergent wetlands-persistent (-2m water?)
+	case 97:
+	    // estuarine emergent wetlands (-2m water)
+	case 98:
+	    // palustrine aquatic bed (-2m water)
+	case 99:
+	    // estuarine aquatic bed (-2m water)
+	    two_layer(x, z, elevval, 2, blockTypes.Dirt, blockTypes.Water)
+		break;
+	default:
+	    //print('unexpected value for land cover: ' + lcval);
+	    one_layer(x, z, elevval, blockTypes.Dirt);
+	    break;
+	}
     }
 }
 
 // fills a column with a base block type then covers it with a block type
 function two_layer(x, z, elevval, depth, baseblockType, topblockType) {
-    underlayer = elevval-depth;
+    var underlayer = elevval-depth;
     map.fillBlocks(x, 1, baseline, filler+underlayer, z, 1, baseblockType);
     map.fillBlocks(x, 1, sealevel+underlayer, depth, z, 1, topblockType);
 }
@@ -185,13 +247,34 @@ function equip_player() {
     map.playerInventory.AddAt(sandstack, 7);
 }
 
+function printLandCoverStatistics(lcType, lcCount) {
+    print('Land cover statistics:');
+    B = [];
+    for (i in lcCount)
+	B.push({v: i, c: lcCount[i]});
+    B.sort(function (a, b) { return b.c - a.c });
+    for (element in B) {
+	i = B[element].v;
+    	if (lcCount[i] > 0) {
+	    lcPercent = Math.round(lcCount[i]/lcTotal*100)/100.00;
+	    print('  '+lcCount[i]+' ('+lcPercent+'%): '+lcType[i]);
+    	}
+    }
+}
+
 function main() {
     print('Begin TopoMC demo');
+    var mainDate = new Date();
+    var startTime = mainDate.getTime();
+
+    // initialize the land cover variables
+    populateLandCoverVariables(lcType, lcCount);
 
     // for loop time!
-    for (row = 0; row < maxrows; row += tilerows) {
-	for (col = 0; col < maxcols; col += tilecols) {
-	    process_image(row, col);
+    // first make sure that minrows and mincols start on tile boundaries
+    for (var row = (minrows - (minrows % tilerows)); row < maxrows; row += tilerows) {
+	for (var col = (mincols - (mincols % tilerows)); col < maxcols; col += tilecols) {
+	    processImage(row, col);
 	}
     }
 
@@ -203,8 +286,14 @@ function main() {
     map.spawn = { x: spawnx, y: spawny+2, z: spawnz };
     map.playerLocation = map.spawn;
 
+    printLandCoverStatistics(lcType, lcCount);
+
     equip_player();
 
+    var mainDate = new Date();
+    var endTime = mainDate.getTime();
+    var mainDelta = Math.round(endTime - startTime)/1000;
+    print('Processing done -- took ' + mainDelta + ' seconds.');
     print('End TopoMC Demo');
 }
 
