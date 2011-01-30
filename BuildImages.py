bf495c6bd504f803fbbd8969bfe4d2c1934ed8de
Jack Twilley <mathuin@gmail.com>
2011-01-30 01:25:27 -0800

    Moved bathymetry to a function, added slope (higher value of slope increases odds that depth won't drop), and changed the single-process version to use the old model to aid in debugging.

--- BuildImages.py
+++ BuildImages.py
@@ -16,6 +16,7 @@ from osgeo.gdalconst import *
 from invdisttree import *
 from multiprocessing import Pool, cpu_count
 from time import time
+from random import random
 gdal.UseExceptions()
 
 # paths for datasets
@@ -179,6 +180,29 @@ def getTileOffsetSize(rowIndex, colIndex, tileShape, maxRows, maxCols, idtPad=0)
     imageSize = (imageRight-imageLeft, imageLower-imageUpper)
     return imageOffset, imageSize
 
+def getBathymetry(lcArray, maxDepth, slope=1):
+    "Generates rough bathymetric values based on proximity to terrain.  Increase slope to decrease dropoff."
+    bathyMaxRows, bathyMaxCols = lcArray.shape
+    bathyArray = numpy.zeros((bathyMaxRows, bathyMaxCols))
+    for brow in xrange(bathyMaxRows):
+        for bcol in xrange(bathyMaxCols):
+            if (lcArray[brow][bcol] == 11):
+                bathyList = [int(bathyArray[x][y]) for x in xrange(max(0,brow-1), min(bathyMaxRows,brow+2)) for y in xrange(max(0,bcol-1), min(bathyMaxCols,bcol+2))]
+                if (all(element == 0 for element in bathyList)):
+                    ringrange = xrange(1,maxDepth)
+                else:
+                    ringrange = xrange(min([elem for elem in bathyList if elem > 0])-1,min(max(bathyList)+2, maxDepth))
+                try:
+                    for ring in ringrange:
+                        if any(lcArray[ringrow][ringcol] != 11 for ringrow in xrange(max(0, brow-ring), min(bathyMaxRows, brow+ring+1)) for ringcol in xrange(max(0, bcol-ring+1), min(bathyMaxCols, bcol+ring+1))):
+                            raise Exception
+                except Exception:
+                    pass
+                if (random() > 1/slope):
+                    ring = ring + 1
+                bathyArray[brow][bcol] = ring
+    return bathyArray
+
 def listDatasets(dsdict):
     "Given a dataset dict, list the datasets and their dimensions."
     print 'Valid datasets detected:'
@@ -305,27 +329,8 @@ def processTile(args, tileShape, mult, vscale, imagedir, tileRowIndex, tileColIn
 
     # TODO: go through the arrays for some special transmogrification
     # first idea: bathymetry
-    maxRing = 10
-    maxRingSeen = 0
-    minRingSeen = 10
-    bathyMaxRows = baseShape[0]
-    bathyMaxCols = baseShape[1]
-    bathyImageArray = numpy.zeros(baseShape)
-    for brow in xrange(bathyMaxRows):
-        for bcol in xrange(bathyMaxCols):
-            if (lcImageArray[brow][bcol] == 11):
-                bathyList = [int(bathyImageArray[x][y]) for x in xrange(max(0,brow-1), min(bathyMaxRows,brow+2)) for y in xrange(max(0,bcol-1), min(bathyMaxCols,bcol+2))]
-                if (all(element == 0 for element in bathyList)):
-                    ringrange = xrange(1,maxRing)
-                else:
-                    ringrange = xrange(min([elem for elem in bathyList if elem > 0])-1,min(max(bathyList)+2, maxRing))
-                try:
-                    for ring in ringrange:
-                        if any(lcImageArray[ringrow][ringcol] != 11 for ringrow in xrange(max(0, brow-ring), min(bathyMaxRows, brow+ring+1)) for ringcol in xrange(max(0, bcol-ring+1), min(bathyMaxCols, bcol+ring+1))):
-                            raise Exception
-                except Exception:
-                    pass
-                bathyImageArray[brow][bcol] = ring
+    # TODO: fix this so it reads idtpadded data
+    bathyImageArray = getBathymetry(lcImageArray, maxDepth=10, slope=1)
     
     # save images
     lcImage = Image.fromarray(lcImageArray)
@@ -383,15 +388,15 @@ def main(argv):
     else:
         os.makedirs(imagedir)
 
-    pool = Pool(processes)
     print "Processing region %s of size (%d, %d) with %d processes..." % (args.region, rows, cols, processes)
 
-    tasks = [(args, tileShape, mult, vscale, imagedir, tileRowIndex, tileColIndex) for tileRowIndex in range(minTileRows, maxTileRows) for tileColIndex in range(minTileCols, maxTileCols)]
-    #[processTile(args, tileShape, mult, vscale, imagedir, tileRowIndex, tileColIndex) for tileRowIndex in range(minTileRows, maxTileRows) for tileColIndex in range(minTileCols, maxTileCols)]
-
-    results = pool.imap_unordered(processTilestar, tasks)
-
-    bleah = [x for x in results]
+    if (processes == 1):
+        [processTile(args, tileShape, mult, vscale, imagedir, tileRowIndex, tileColIndex) for tileRowIndex in range(minTileRows, maxTileRows) for tileColIndex in range(minTileCols, maxTileCols)]
+    else:
+        pool = Pool(processes)
+        tasks = [(args, tileShape, mult, vscale, imagedir, tileRowIndex, tileColIndex) for tileRowIndex in range(minTileRows, maxTileRows) for tileColIndex in range(minTileCols, maxTileCols)]
+        results = pool.imap_unordered(processTilestar, tasks)
+        bleah = [x for x in results]
             
     print "Render complete -- total array of %d tiles was %d x %d" % ((maxTileRows-minTileRows)*(maxTileCols-minTileCols), int(rows*mult), int(cols*mult))
 
