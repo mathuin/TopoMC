22f65ae87133bd3c7005e9aacc8c7abad01b0a59
Jack Twilley <mathuin@gmail.com>
2011-02-04 12:37:37 -0800

    oops

--- BuildWorld.py
+++ BuildWorld.py
@@ -560,7 +560,7 @@ def main(argv):
     massarraydata = numpy.empty_like(massarray)
 
     # opening the world
-    #worlddir = "/home/jmt/.minecraft/saves/World%d" % mainargs.world
+    worlddir = "/home/jmt/.minecraft/saves/World%d" % mainargs.world
     #rmtree(worlddir)
     #os.mkdir(worlddir)
     world = mclevel.MCInfdevOldLevel(worlddir)
