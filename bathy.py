34827159c5850502190e99d1d29dd63882700283
Jack Twilley <mathuin@gmail.com>
2011-02-06 20:01:12 -0800

    Converted first of two slow parts of bathy.py.

--- bathy.py
+++ bathy.py
@@ -10,15 +10,29 @@ def getBathymetry(lcArray, maxDepth, slope=1):
     bathyArray = numpy.zeros((bathyMaxRows, bathyMaxCols))
     for brow, bcol in product(xrange(bathyMaxRows), xrange(bathyMaxCols)):
         if (lcArray[brow,bcol] == 11):
-            bathyList = [int(bathyArray[x,y]) for x in xrange(max(0,brow-1), min(bathyMaxRows,brow+2)) for y in xrange(max(0,bcol-1), min(bathyMaxCols,bcol+2))]
+            bxmin = max(0, brow-1)
+            bxmax = min(bathyMaxRows,brow+2)
+            bzmin = max(0, bcol-1)
+            bzmax = min(bathyMaxCols,bcol+1)
+            barray = bathyArray[bxmin:bxmax,bzmin:bzmax].flatten()
+            bathyList = [int(x) for x in barray]
             if (all(element == 0 for element in bathyList)):
                 ringrange = xrange(1,maxDepth)
             else:
                 ringrange = xrange(min([elem for elem in bathyList if elem > 0])-1,min(max(bathyList)+2, maxDepth))
             try:
                 for ring in ringrange:
-                    if any(lcArray[ringrow,ringcol] != 11 for ringrow in xrange(max(0, brow-ring), min(bathyMaxRows, brow+ring+1)) for ringcol in xrange(max(0, bcol-ring+1), min(bathyMaxCols, bcol+ring+1))):
-                        raise Exception
+                    if (True):
+                        if any(lcArray[ringrow,ringcol] != 11 for ringrow in xrange(max(0, brow-ring+1), min(bathyMaxRows, brow+ring+1)) for ringcol in xrange(max(0, bcol-ring+1), min(bathyMaxCols, bcol+ring+1))):
+                            raise Exception
+                    else:
+                        xmin = max(0, brow-ring+1)
+                        xmax = min(bathyMaxRows, brow+ring+1)
+                        zmin = max(0, bcol-ring+1)
+                        zmax = min(bathyMaxCols, bcol+ring+1)
+                        ringarray = lcArray[xmin:xmax,zmin:zmax]
+                        if any(x != 11 for x in ringarray):
+                            raise Exception
             except Exception:
                 pass
             if (random() > 1/slope):
