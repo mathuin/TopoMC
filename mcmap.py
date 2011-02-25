cbaab4bfea76cb09a65da33ed639e0827a56a542
Jack Twilley <mathuin@gmail.com>
2011-02-24 21:02:12 -0800

    abstracted out constants, began attempts at placing bedrock, migrated certain values to global variables

--- mcmap.py
+++ mcmap.py
@@ -11,21 +11,45 @@ from numpy import zeros, uint8
 from itertools import product
 from random import randint
 
+# level constants
+chunkWidthPow = 4
+chunkWidth = pow(2,chunkWidthPow)
+chunkHeight = 128
 # constants
 sealevel = 64
 # headroom is the room between the tallest peak and the ceiling
 headroom = 10
-maxelev = 128-headroom-sealevel
+maxelev = chunkHeight-headroom-sealevel
+
+# variables
+minX = 0
+minZ = 0
+maxX = 0
+maxZ = 0
+processes = 0
 
 makeWorldNow = False
-# FIXME: add map extents here
-# minx, maxx, minz, maxz
-def scaffoldWorld(minX, minZ, maxX, maxZ):
+
+def initWorld(string, wminX, wminZ, wmaxX, wmaxZ, wprocesses):
+    "Open this world."
+    global world, minX, minZ, maxX, maxZ, processes
+    # set defaults
+    minX = wminX
+    minZ = wminZ
+    maxX = wmaxX
+    maxZ = wmaxZ
+    processes = wprocesses
+    try:
+        worldNum = int(string)
+    except ValueError:
+        world = mclevel.MCInfdevOldLevel(string, create=True)
+    else:
+        world = mclevel.loadWorldNumber(worldNum)
     # for now, assume minX and minZ are zero
-    minXchunk = (minX >> 4)
-    minZchunk = (minZ >> 4)
-    maxXchunk = (maxX >> 4)
-    maxZchunk = (maxZ >> 4)
+    minXchunk = (minX >> chunkWidthPow)
+    minZchunk = (minZ >> chunkWidthPow)
+    maxXchunk = (maxX >> chunkWidthPow)
+    maxZchunk = (maxZ >> chunkWidthPow)
     chunkX = xrange(minXchunk-1, maxXchunk+2)
     chunkZ = xrange(minZchunk-1, maxZchunk+2)
     for x, z in product(chunkX, chunkZ):
@@ -36,8 +60,8 @@ def scaffoldWorld(minX, minZ, maxX, maxZ):
             except mclevel.ChunkNotPresent:
                 world.createChunk(z, x)
             world.compressChunk(z, x)
-        arrayBlocks[arrayKey] = SharedMemArray(zeros((16,16,128),dtype=uint8))
-        arrayData[arrayKey] = SharedMemArray(zeros((16,16,128),dtype=uint8))
+        arrayBlocks[arrayKey] = SharedMemArray(zeros((chunkWidth,chunkWidth,chunkHeight),dtype=uint8))
+        arrayData[arrayKey] = SharedMemArray(zeros((chunkWidth,chunkWidth,chunkHeight),dtype=uint8))
 
 # each column consists of [x, z, elevval, ...]
 # where ... is a block followed by zero or more number-block pairs
@@ -48,9 +72,7 @@ def scaffoldWorld(minX, minZ, maxX, maxZ):
 #  - elevval down two levels of water, rest dirt
 # [x, y, elevval, 'Stone', 1, 'Dirt', 1, 'Water']
 #  - elevval down one level of water, then one level of dirt, then stone
-# NB: we currently push 'Stone' here since every layer ends in it.
-# INSANE: generate an invdistree with world-wide randomness
-# INSANER: scale for depth -- maxdepth ocean gets none
+# We add the final values here so the hard floor is 'Bedrock'.
 def layers(columns):
     blocks = []
     for column in columns:
@@ -58,6 +80,10 @@ def layers(columns):
         z = column.pop(0)
         elevval = column.pop(0)
         top = sealevel+elevval
+        # overstone = sum([elem for elem in column if type(elem) == int])
+        # column.insert(0, 'Bedrock')
+        # column.insert(1, top-overstone-1)
+        # column.insert(2, 'Stone')
         column.insert(0, 'Stone')
         while (len(column) > 0 or top > 0):
             # better be a block
@@ -91,55 +117,60 @@ def fillBlocks(ix, dx, iy, dy, iz, dz, block, data=None):
 # my own setblockat
 def setBlockAt(x, y, z, string):
     global arrayBlocks
-    arrayKey = '%d,%d' % (x >> 4, z >> 4)
+    arrayKey = '%d,%d' % (x >> chunkWidthPow, z >> chunkWidthPow)
     myBlocks = arrayBlocks[arrayKey].asarray()
-    myBlocks[x & 0xf, z & 0xf, y] = materials.materialNamed(string)
+    myBlocks[x & chunkWidth-1, z & chunkWidth-1, y] = materials.materialNamed(string)
 
 # more aggregates
 def setBlocksAt(blocks):
     global arrayBlocks
     for block in blocks:
         (x, y, z, string) = block
-        arrayKey = '%d,%d' % (x >> 4, z >> 4)
+        arrayKey = '%d,%d' % (x >> chunkWidthPow, z >> chunkWidthPow)
         myBlocks = arrayBlocks[arrayKey].asarray()
-        myBlocks[x & 0xf, z & 0xf, y] = materials.materialNamed(string)
+	try:
+	    materials.materialNamed(string)
+	except ValueError:
+	    print "block not found: %s" % string
+	else:
+            myBlocks[x & chunkWidth-1, z & chunkWidth-1, y] = materials.materialNamed(string)
 
 # my own setblockdataat
 def setBlockDataAt(x, y, z, data):
     global arrayData
-    arrayKey = '%d,%d' % (x >> 4, z >> 4)
+    arrayKey = '%d,%d' % (x >> chunkWidthPow, z >> chunkWidthPow)
     myData = arrayData[arrayKey].asarray()
-    myData[x & 0xf, z & 0xf, y] = data
+    myData[x & chunkWidth-1, z & chunkWidth-1, y] = data
 
 # my own setblocksdataat
 def setBlocksDataAt(blocks):
     global arrayData
     for block in blocks:
         (x, y, z, data) = block
-        arrayKey = '%d,%d' % (x >> 4, z >> 4)
+        arrayKey = '%d,%d' % (x >> chunkWidthPow, z >> chunkWidthPow)
         myData = arrayData[arrayKey].asarray()
-        myData[x & 0xf, z & 0xf, y] = data
+        myData[x & chunkWidth-1, z & chunkWidth-1, y] = data
 
 def getBlockAt(x, y, z):
     "Returns the block ID of the block at this point."
-    arrayKey = '%d,%d' % (x >> 4, z >> 4)
+    arrayKey = '%d,%d' % (x >> chunkWidthPow, z >> chunkWidthPow)
     myBlocks = arrayBlocks[arrayKey].asarray()
-    return materials.names[myBlocks[x & 0xf, z & 0xf, y]]
+    return materials.names[myBlocks[x & chunkWidth-1, z & chunkWidth-1, y]]
     
 def getBlockDataAt(x, y, z):
     "Returns the data value of the block at this point."
-    arrayKey = '%d,%d' % (x >> 4, z >> 4)
+    arrayKey = '%d,%d' % (x >> chunkWidthPow, z >> chunkWidthPow)
     myData = arrayData[arrayKey].asarray()
-    return myData[x & 0xf, z & 0xf, y]
+    return myData[x & chunkWidth-1, z & chunkWidth-1, y]
 
 def getBlocksAt(blocks):
     "Returns the block names of the blocks in the list."
     retval = []
     for block in blocks:
         (x, y, z) = block
-        arrayKey = '%d,%d' % (x >> 4, z >> 4)
+        arrayKey = '%d,%d' % (x >> chunkWidthPow, z >> chunkWidthPow)
         myBlocks = arrayBlocks[arrayKey].asarray()
-        retval.append(materials.names[myBlocks[x & 0xf, z & 0xf, y]])
+        retval.append(materials.names[myBlocks[x & chunkWidth-1, z & chunkWidth-1, y]])
     return retval
 
 def populateChunk(key,maxcz):
@@ -156,22 +187,22 @@ def populateChunk(key,maxcz):
     except mclevel.ChunkNotPresent:
         world.createChunk(cx, cz)
     chunk = world.getChunk(cx, cz)
-    world.compressChunk(cx, cz)
+    #world.compressChunk(cx, cz)
     myBlocks = arrayBlocks[key].asarray()
     myData = arrayData[key].asarray()
-    for x, z in product(xrange(16), xrange(16)):
-        chunk.Blocks[x,z] = myBlocks[15-z,x]
+    for x, z in product(xrange(chunkWidth), xrange(chunkWidth)):
+        chunk.Blocks[x,z] = myBlocks[chunkWidth-1-z,x]
     if key in arrayData:
-        for x, z in product(xrange(16), xrange(16)):
-            chunk.Data[x,z] = myData[15-z,x]
+        for x, z in product(xrange(chunkWidth), xrange(chunkWidth)):
+            chunk.Data[x,z] = myData[chunkWidth-1-z,x]
     chunk.chunkChanged(False)
     return (clock()-start)
 
 def populateChunkstar(args):
     return populateChunk(*args)
 
-def populateWorld(processes,minX,maxX):
-    maxcx = (maxX-minX) >> 4
+def populateWorld():
+    maxcx = (maxX-minX) >> chunkWidthPow
     # FIXME: still no multiprocessing support but less important
     if (processes == 1 or True):
         times = [populateChunk(key,maxcx) for key in arrayBlocks.keys()]
@@ -204,18 +235,7 @@ def checkWorld(string):
             raise IOError, "bad value for world: %s" % string
     return string
 
-def initWorld(string):
-    "Open this world."
-    global world
-    try:
-        worldNum = int(string)
-    except ValueError:
-        myworld = mclevel.MCInfdevOldLevel(string, create=True)
-    else:
-        myworld = mclevel.loadWorldNumber(worldNum)
-    return myworld
-
-def saveWorld(spawn, minX, maxX):
+def saveWorld(spawn):
     global world
     sizeOnDisk = 0
     # adjust it to sealevel, and then up another two for good measure
