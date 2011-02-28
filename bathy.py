559add98dfc5c6e9357bbec0676185d0eb7fe643
Jack Twilley <mathuin@gmail.com>
2011-02-27 17:38:14 -0800

    use new bathy function and added new maxdepth constraint

--- bathy.py
+++ bathy.py
@@ -5,36 +5,42 @@ from itertools import product
 from random import random
 from dataset import getDatasetDims
 from terrain import nodata
-
-useNew = True
+from math import hypot
+import invdisttree
+from mcmap import sealevel
 
 def getBathymetry(lcArray, bigArray, baseOffset, bigOffset, maxDepth, slope=1):
     "Generates rough bathymetric values based on proximity to terrain.  Increase slope to decrease dropoff."
+    # what is water?
+    setWater = set([11, 12])
+    if (nodata in setWater):
+        setWater.update([127])
+    # build an lc invdisttree *without* setWater values
     bathyMaxRows, bathyMaxCols = lcArray.shape
     bigMaxRows, bigMaxCols = bigArray.shape
     xDiff = baseOffset[1]-bigOffset[1]
     zDiff = baseOffset[0]-bigOffset[0]
     bathyArray = zeros((bathyMaxRows, bathyMaxCols),dtype=uint8)
-    ringrange = xrange(1,maxDepth+1)
-    setWater = set([11, 12])
-    if (nodata in setWater):
-        setWater.update([127])
-    for brow, bcol in product(xrange(bathyMaxRows), xrange(bathyMaxCols)):
-        if (lcArray[brow,bcol] in setWater):
-            try:
-                for ring in ringrange:
-                    rbxmin = (brow-ring+1)+xDiff
-                    rbxmax = (brow+ring+1)+xDiff
-                    rbzmin = (bcol-ring+1)+zDiff
-                    rbzmax = (bcol+ring+1)+zDiff
-                    ringset = set(bigArray[rbxmin:rbxmax,rbzmin:rbzmax].flatten())
-                    if len(ringset.difference(setWater)) > 0:
-                        raise Exception
-            except Exception:
-                pass
-            if (random() > 1/slope):
-                ring = ring + 1
-            bathyArray[brow,bcol] = ring
+    bigDry = [[x,z] for x,z in product(xrange(bigMaxRows), xrange(bigMaxCols)) if bigArray[x,z] not in setWater]
+    # if there's no land at all... 
+    if (len(bigDry) == 0):
+        bathyArray += maxDepth
+        return bathyArray
+    bigXValues = [x for x,z in bigDry]
+    bigZValues = [z for x,z in bigDry]
+    bigXIDT = invdisttree.Invdisttree(bigDry, bigXValues)
+    bigZIDT = invdisttree.Invdisttree(bigDry, bigZValues)
+    bigFull = [[x,z] for x,z in product(xrange(bigMaxRows), xrange(bigMaxCols))]
+    bigXNear = bigXIDT(bigFull, nnear=1, eps=0.1, majority=False)
+    bigXNear.resize(bigArray.shape)
+    bigZNear = bigZIDT(bigFull, nnear=1, eps=0.1, majority=False)
+    bigZNear.resize(bigArray.shape)
+    bigDepth = zeros((bigMaxRows, bigMaxCols), dtype=uint8)
+    for x, z in product(xrange(xDiff,bathyMaxRows+xDiff), xrange(zDiff,bathyMaxCols+zDiff)):
+        if lcArray[x-xDiff, z-zDiff] in setWater:
+            bigX = bigXNear[x, z]
+            bigZ = bigZNear[x, z]
+            bathyArray[x-xDiff, z-zDiff] = min(maxDepth,hypot((bigX-x), (bigZ-z)))
     return bathyArray
 
 def checkMaxDepth(args):
@@ -46,8 +52,9 @@ def checkMaxDepth(args):
     (rows, cols) = getDatasetDims(args.region)
     # okay, 1 is a minimum
     # rows/cols is a max
+    # actually sealevel-1 is a real max! :-)
     maxdepth = max(1, oldmaxdepth)
-    maxdepth = min(maxdepth, min(rows, cols))
+    maxdepth = min(maxdepth, min(rows, cols, sealevel-1))
     if (maxdepth != oldmaxdepth):
         print "Warning: maximum depth of %d for region %s is invalid -- changed to %d" % (oldmaxdepth, args.region, maxdepth)
     args.maxdepth = maxdepth
